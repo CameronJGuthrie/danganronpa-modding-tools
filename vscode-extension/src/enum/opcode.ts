03f1cda6da8256c0a4d8b85bf358141afd4e3d95
CameronJGuthrie <cameronguthrie.nz@gmail.com>
2025-11-29 13:34:05 +1300

    remove unsed opcode

--- vscode-extension/src/enum/opcode.ts
+++ vscode-extension/src/enum/opcode.ts
@@ -43,7 +43,6 @@ export enum OpcodeName {
   Sprite = "Sprite",
   SpriteFlash = "SpriteFlash",
   StudentRelationship = "StudentRelationship",
-  StudentReportCard = "StudentReportCard",
   StudentReportInfo = "StudentReportInfo",
   StudentTitleEntry = "StudentTitleEntry",
   Text = "Text",
